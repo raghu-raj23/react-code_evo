659fcf2bd5ebaa1ac0ba7a459c3195cc8f9ecf8b
raghu-raj23 <raghuraj.muni@gmail.com>
2021-11-10 21:12:29 +0530

    useContext Hook

--- hooks-tut/src/App.js
+++ hooks-tut/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import React from 'react';
+
 import L1_Counter from './components/L1_Counter';
 import L2_CounterWithFunc from './components/L2_CounterWithFunc';
 import L3_CounterWithSpread from './components/L3_CounterWithSpread';
@@ -8,6 +10,11 @@ import L6_HookMouse from './components/L6_HookMouse';
 import L7_MouseContainer from './components/L7_MouseContainer';
 import L8_IntervalHookCounter from './components/L8_IntervalHookCounter';
 import L9_DataFetchingAxios from './components/L9_DataFetchingAxios';
+import L10_ContextCompC from './components/L10_ContextCompC';
+
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
+
 function App() {
   return (
     <div className="App">
@@ -20,6 +27,13 @@ function App() {
       <L7_MouseContainer />
       <L8_IntervalHookCounter />
       <L9_DataFetchingAxios />
+
+      <UserContext.Provider value={'Raghuraj'}>
+        <ChannelContext.Provider value={'Learning Hooks'}>
+          <L10_ContextCompC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
+      
     </div>
   );
 }
---
+++ hooks-tut/src/components/L10_ContextCompC.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import L10_ContextCompD from './L10_ContextCompD'
+
+function L10_ContextCompC() {
+
+    return (
+        <div>
+            <L10_ContextCompD />
+        </div>
+    )
+}
+
+export default L10_ContextCompC
