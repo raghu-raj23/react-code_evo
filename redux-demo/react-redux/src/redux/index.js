ee6f3108771fd41f83d1bbefaf442e20f3e8432a
raghu-raj23 <raghuraj.muni@gmail.com>
2023-08-01 21:07:12 +0530

    completed react nd react hoks

--- redux-demo/react-redux/src/redux/index.js
+++
@@ -1,4 +0,0 @@
-export { buyCake } from './cake/cakeActions';
-export { buyIceCream } from './iceCream/iceCreamActions';
-export * from './user/userActions';
-// this export all the action creators from userActions.js
